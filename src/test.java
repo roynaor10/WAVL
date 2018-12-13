e1461e05440ab02327dccc6224f4008ab35b9547
roynaor10 <naor.roy@gmail.com>
2018-12-14 00:05:44 +0200

    fixed code duplication

--- src/test.java
+++ src/test.java
@@ -4,18 +4,26 @@ public class test {
 
 	public static void main(String[] args) {
 		WAVLTree wTree=new WAVLTree();
-		wTree.insert(20, "please work"); 
-		wTree.insert(5, "there"); 
-		wTree.insert(10, "hello"); 
-		wTree.insert(15, "ther6e"); 
-		wTree.insert(30, "there"); 
-		wTree.insert(25, "there"); 
-		wTree.insert(35, "there"); 
-
-		System.out.println(wTree.size());
+		wTree.insert(35, "please work"); 
 		System.out.println(Arrays.toString(wTree.keysToArray()));
+		wTree.insert(30, "there"); 
+		System.out.println(Arrays.toString(wTree.keysToArray()));
+		wTree.insert(25, "hello"); 
+		System.out.println(Arrays.toString(wTree.keysToArray()));
+		wTree.insert(20, "ther6e"); 
+		System.out.println(Arrays.toString(wTree.keysToArray()));
+		wTree.insert(15, "there"); 
+		System.out.println(Arrays.toString(wTree.keysToArray()));
+		System.out.println(wTree.getRoot().getRight().getKey());
+		wTree.insert(10, "there"); 
+		System.out.println(Arrays.toString(wTree.keysToArray()));
+		wTree.insert(5, "there"); 
+		System.out.println(Arrays.toString(wTree.keysToArray()));
+
+		//System.out.println(wTree.size());
+		//System.out.println(Arrays.toString(wTree.keysToArray()));
 		//System.out.println(wTree.getRoot().getKey());
-		System.out.println(wTree.getRoot().getRight().getRight().getKey());
+		//System.out.println(wTree.getRoot().getRight().getRight().getKey());
 
 	}
 
