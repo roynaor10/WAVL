6867d9ad9b329574db1daf3ae4649baa893eb7df
aradweit <45330072+arweit@users.noreply.github.com>
2018-12-20 16:43:52 +0200

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,32 +1,36 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+ 
 
 public class test {
 
 	public static void main(String[] args) {
 		WAVLTree wTree=new WAVLTree();
-
-
-		wTree.insert(20, "5"); 
-		wTree.insert(10, "4"); 
-		wTree.insert(30, "3"); 
-		wTree.insert(5, "2"); 
-		wTree.insert(15, "1"); 	
-		wTree.insert(25, "1"); 	
-		wTree.insert(35, "1"); 	
-		wTree.insert(40, "1"); 	
-		wTree.delete(35);
-		wTree.delete(30);
-		wTree.delete(25);
-		wTree.delete(5);
-		wTree.delete(20);
-		wTree.delete(15);
-		wTree.delete(40);
-		wTree.delete(10);
-		//System.out.println(wTree.getRoot().getKey());
+		Random rand = new Random();
+        Set<Integer> set = new HashSet<Integer>(); 
+        
+        wTree.insert(-4, "-4"); 
+		
+		for (int i = 1; i <= 200000; i++) {
+			int x = rand.nextInt(200000000); 
+			set.add(x); 
+			wTree.insert(x, Integer.toString(x)); 
+		}
+		
+		for (int x : set) {
+			wTree.delete(x); 
+		}
+	
+		
+		
+		
+		System.out.println("********************************");
+		System.out.println(wTree.getRoot().getKey()); 
+		System.out.println("********************************");
 		System.out.println(wTree.size());
 		System.out.println(Arrays.toString(wTree.keysToArray()));
-		//System.out.println(Arrays.toString(wTree.infoToArray())); 
-
-
+		System.out.println(Arrays.toString(wTree.infoToArray())); 
 	}
 }
